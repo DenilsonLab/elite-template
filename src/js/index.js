4836b164474b2cfaefad329581c400f26e98529a
Denilson (Rewrite) <denilsonn1998@gmail.com>
2021-04-23 07:08:18 -0400

    My first commit

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+﻿$(".skill-per").each(function() {
+    var $this = $(this);
+    var per = $this.attr("per");
+    $this.css("width", per + "%");
+    $({ animatedValue: 0 }).animate(
+      { animatedValue: per },
+      {
+        duration: 1000,
+        step: function() {
+          $this.attr("per", Math.floor(this.animatedValue) + "%");
+        },
+        complete: function() {
+          $this.attr("per", Math.floor(this.animatedValue) + "%");
+        }
+      }
+    );
+  });
+  
+
+
+  // Portfolio filters
+  $('.filters ul li').click(function(){
+    $('.filters ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var data = $(this).attr('data-filter');
+    $grid.isotope({
+      filter: data
+    })
+  });
+  
+  var $grid = $(".grid").isotope({
+    itemSelector: ".all",
+    percentPosition: true,
+    masonry: {
+      columnWidth: ".all"
+    }
+  })
